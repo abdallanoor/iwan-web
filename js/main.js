71cb54cca2639d05ec5e359534e6bc14d0abef99
Abdallah Noor <125575767+abdallanoor@users.noreply.github.com>
2023-04-04 00:14:23 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,97 @@
-/* --------------------------------- navBar --------------------------------- */
+/* --------------------------------- Loader --------------------------------- */
+window.addEventListener("load", () => {
+  const spinner = document.querySelector(".spinner");
+  spinner.classList.add("hide-loader");
+
+  spinner.addEventListener("transitioned", () => {
+    document.body.removeChild("spinner");
+  });
+});
+/* --------------------------------- Loader --------------------------------- */
+/* --------------------------------- Nav Bar --------------------------------- */
 var navTrigger = document.querySelector(".navTrigger");
 var navBar = document.querySelector(".nav-bar");
 navTrigger.onclick = function () {
   navTrigger.classList.toggle("active");
   navBar.classList.toggle("show");
 };
-/* --------------------------------- navBar --------------------------------- */
-/* ---------------------------------- FAQS ---------------------------------- */
-// W3C's JS Code
+/* --------------------------------- Nav Bar --------------------------------- */
+/* ------------------------------ Pricing page ------------------------------ */
+var tabs = document.querySelectorAll(".tabs li");
+var contents = document.querySelectorAll(".price-content");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    // to remove uk-active class from previous tab
+    tabs.forEach((tab) => tab.classList.remove("uk-active"));
+    tab.classList.add("uk-active");
+    // to show content according to tab selected
+
+    // to hide previous tab content
+    contents.forEach((c) => c.classList.remove("uk-active"));
+    contents[index].classList.add("uk-active");
+  });
+});
+/* ------------------------------ Pricing page ------------------------------ */
+/* ------------------------------- Send Email ------------------------------- */
+var yourName = document.getElementById("yourName");
+var email = document.getElementById("email");
+var phone = document.getElementById("phone");
+var message = document.getElementById("message");
+var massageAccept = document.querySelector(".massage-accept");
+
+function sendEmail() {
+  Email.send({
+    SecureToken: "99668455-af99-44e8-962f-ed146b732187",
+    To: "abdallaahnoor@gmail.com",
+    From: "abdallahabdelaziznoor@gmail.com",
+    Subject: "Contact Message",
+    Body:
+      "Name: " +
+      yourName.value +
+      "<br> Email: " +
+      email.value +
+      "<br> Phone Number: " +
+      phone.value +
+      "<br> Message: " +
+      message.value,
+  }).then((message) => massageAccept.classList.add("show"));
+}
+/* ------------------------------- Send Email ------------------------------- */
+/* -------------------------------- Swiper JS ------------------------------- */
+var swiper = new Swiper(".mySwiper", {
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+/* -------------------------------- Swiper JS ------------------------------- */
+/* ---------------------------------- Accordion ---------------------------------- */
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + "px";
+//     }
+//   });
+// }
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -46,7 +130,6 @@ for (i = 0; i < acc.length; i++) {
   };
 }
 
-// Cross-browser way to get the computed height of a certain element. Credit to @CMS on StackOverflow (http://stackoverflow.com/a/2531934/7926565)
 function getStyle(el, styleProp) {
   var value,
     defaultView = (el.ownerDocument || document).defaultView;
@@ -79,51 +162,7 @@ function getStyle(el, styleProp) {
     return value;
   }
 }
-/* ---------------------------------- FAQS ---------------------------------- */
-/* ------------------------------ Pricing page ------------------------------ */
-var tabs = document.querySelectorAll(".tabs li");
-var contents = document.querySelectorAll(".price-content");
-
-tabs.forEach((tab, index) => {
-  tab.addEventListener("click", () => {
-    // to remove uk-active class from previous tab
-    tabs.forEach((tab) => tab.classList.remove("uk-active"));
-    tab.classList.add("uk-active");
-    // to show content according to tab selected
-
-    // to hide previous tab content
-    contents.forEach((c) => c.classList.remove("uk-active"));
-    contents[index].classList.add("uk-active");
-  });
-});
-// to run the animation initially when the page load
-// tabs[0].click();
-/* ------------------------------ Pricing page ------------------------------ */
-/* ------------------------------- Send Email ------------------------------- */
-var yourName = document.getElementById("yourName");
-var email = document.getElementById("email");
-var phone = document.getElementById("phone");
-var message = document.getElementById("message");
-var massageAccept = document.querySelector(".massage-accept");
-
-function sendEmail() {
-  Email.send({
-    SecureToken: "99668455-af99-44e8-962f-ed146b732187",
-    To: "abdallaahnoor@gmail.com",
-    From: "abdallahabdelaziznoor@gmail.com",
-    Subject: "Contact Message",
-    Body:
-      "Name: " +
-      yourName.value +
-      "<br> Email: " +
-      email.value +
-      "<br> Phone Number: " +
-      phone.value +
-      "<br> Message: " +
-      message.value,
-  }).then((message) => massageAccept.classList.add("show"));
-}
-/* ------------------------------- Send Email ------------------------------- */
+/* ---------------------------------- Accordion ---------------------------------- */
 /* ---------------------------------- go up --------------------------------- */
 var goUp = document.querySelector(".go-up");
 window.onscroll = function () {
@@ -151,22 +190,3 @@ close.onclick = function () {
   whatsappWidget.classList.remove("show");
 };
 /* -------------------------------- WhatsApp -------------------------------- */
-/* -------------------------------- Swiper JS ------------------------------- */
-var swiper = new Swiper(".mySwiper", {
-  grabCursor: true,
-  centeredSlides: true,
-  loop: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-/* -------------------------------- Swiper JS ------------------------------- */
